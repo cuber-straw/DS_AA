4863d1fab5e6be43f361a8072f844665f1dc3027
qiaodisheng <181850128@smail.nju.edu.cn>
2019-10-24 18:39:01 +0800

    initial commit

--- src/chapter3_2/JosephusArray.java
+++ src/chapter3_2/JosephusArray.java
@@ -2,13 +2,13 @@ package chapter3_2;
 
 public class JosephusArray {
     public static int josephus(int n, int m){
-        boolean[] alive = new boolean[n];
+        boolean[] alive = new boolean[n]; // 布尔值为false表示已被淘汰
         for (int i=0; i<n; i++){
             alive[i] = true;
         }
         int numOfAlivePeople = n; // 标记场上还有几人未被淘汰
-        int index = 0;
-        int count = 1;
+        int index = 0; // 指向当前应该报数的人的下标
+        int count = 1; // 当前被指向的人应该报的数
         while (true){
 
             // 当前序号的人已经淘汰,那么就继续检查下一个人，直到找到没有淘汰的人
@@ -23,7 +23,7 @@ public class JosephusArray {
             if (count == m){
                 alive[index] = false;
                 numOfAlivePeople --;
-                if (numOfAlivePeople == 0){
+                if (numOfAlivePeople == 0){ // 当前index指向的人是最后一个被淘汰的，即赢家
                     return index+1;
                 }
                 count = 1;
@@ -32,7 +32,7 @@ public class JosephusArray {
                     index = 0;
                 }
             }
-            // 当前序号的人不会被淘汰
+            // 当前序号的人不被淘汰，继续下一个人报数
             else {
                 index ++;
                 if (index == n){
